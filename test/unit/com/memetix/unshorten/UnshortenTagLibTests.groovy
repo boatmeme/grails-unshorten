a8b168d5af3aac8d80f3d08cc5b10bb928361bbd
Jonathan Griggs <jonathan.griggs@gmail.com>
2011-05-26 12:25:27 -0600

    Brought Unit Tests to 1.4.0 Compliance

---
+++ test/unit/com/memetix/unshorten/UnshortenTagLibTests.groovy
@@ -0,0 +1,71 @@
+package com.memetix.unshorten
+
+import grails.test.mixin.*
+import static org.junit.Assert.*
+import org.junit.*
+
+@TestFor(UnshortenTagLib)
+@Mock(UnshortenTagLib)
+class UnshortenTagLibTests  {
+    
+    @Before
+    public void setUp() {
+        defineBeans {
+           unshortenService(UnshortenService) 
+        }
+    }
+
+    @After
+    public void tearDown() {
+        
+    }
+
+    void testExpandUrls() {
+        setUp()
+        def template = "<unshorten:expandUrls>On a more pleasant note, pets and kids in baskets are a very popular photo subject. I could make a collection. http://ff.im/-DlkAq http://www.the-grotto.com/tits.net http://minu.me/4fmw hahaha</unshorten:expandUrls>"
+        assertEquals( 'On a more pleasant note, pets and kids in baskets are a very popular photo subject. I could make a collection. http://www.twitcaps.com/search?q=basket# http://www.the-grotto.com/tits.net http://www.twitcaps.com/search?q=dsk hahaha', 
+            applyTemplate(template) )
+    }
+   
+    
+    void testExpandAndLinkUrls() {
+        def template = "<unshorten:expandAndLinkUrls>On a more pleasant note, pets and kids in baskets are a very popular photo subject. I could make a collection. http://ff.im/-DlkAq http://www.the-grotto.com/tits.net http://minu.me/4fmw hahaha</unshorten:expandAndLinkUrls>"
+        assertEquals( 'On a more pleasant note, pets and kids in baskets are a very popular photo subject. I could make a collection. <a href="http://www.twitcaps.com/search?q=basket#">http://www.twitcaps.com/search?q=basket#</a> <a href="http://www.the-grotto.com/tits.net">http://www.the-grotto.com/tits.net</a> <a href="http://www.twitcaps.com/search?q=dsk">http://www.twitcaps.com/search?q=dsk</a> hahaha', 
+            applyTemplate(template) )
+    }
+    
+    void testExpandAndLinkUrlsSame() {
+        def template = "<unshorten:expandAndLinkUrls>On a more pleasant note, http://bit.ly/mtXafs 1 http://bit.ly/mtXafs 2 http://bit.ly/mtXafs 3 http://bit.ly/mtXafs 4</unshorten:expandAndLinkUrls>"
+        assertEquals( 'On a more pleasant note, <a href="http://www.careerrocketeer.com/2011/05/are-you-promotable.html">http://www.careerrocketeer.com/2011/05/are-you-promotable.html</a> 1 <a href="http://www.careerrocketeer.com/2011/05/are-you-promotable.html">http://www.careerrocketeer.com/2011/05/are-you-promotable.html</a> 2 <a href="http://www.careerrocketeer.com/2011/05/are-you-promotable.html">http://www.careerrocketeer.com/2011/05/are-you-promotable.html</a> 3 <a href="http://www.careerrocketeer.com/2011/05/are-you-promotable.html">http://www.careerrocketeer.com/2011/05/are-you-promotable.html</a> 4', 
+            applyTemplate(template) )
+    }
+    
+    void testExpandAndLinkUrlsClass() {
+        def myClass = "myLinkClass"
+        def template = "<unshorten:expandAndLinkUrls linkClass='${myClass}'>On a more pleasant note, pets and kids in baskets are a very popular photo subject. I could make a collection. http://ff.im/-DlkAq http://www.the-grotto.com/tits.net http://minu.me/4fmw hahaha</unshorten:expandAndLinkUrls>"
+        assertEquals( 'On a more pleasant note, pets and kids in baskets are a very popular photo subject. I could make a collection. <a class="myLinkClass" href="http://www.twitcaps.com/search?q=basket#">http://www.twitcaps.com/search?q=basket#</a> <a class="myLinkClass" href="http://www.the-grotto.com/tits.net">http://www.the-grotto.com/tits.net</a> <a class="myLinkClass" href="http://www.twitcaps.com/search?q=dsk">http://www.twitcaps.com/search?q=dsk</a> hahaha', 
+            applyTemplate(template) )
+    }
+    
+    void testUnshortenUrl() {
+        def myUrl = 'http://ff.im/-DlkAq'
+        def template = "<unshorten:unshortenUrl url='${myUrl}'/>"
+        assertEquals( 'http://www.twitcaps.com/search?q=basket#', 
+            applyTemplate(template) )
+    }
+    
+    void testUnshortenAndLinkUrl() {
+        def myUrl = 'http://ff.im/-DlkAq'
+        def template = "<unshorten:unshortenAndLinkUrl url='${myUrl}'/>"
+        assertEquals( '<a href="http://www.twitcaps.com/search?q=basket#">http://www.twitcaps.com/search?q=basket#</a>', 
+            applyTemplate(template) )
+    }
+    
+    void testUnshortenAndLinkUrlClass() {
+        def myUrl = 'http://ff.im/-DlkAq'
+        def myClass  = 'myLinkClass'
+        def template = "<unshorten:unshortenAndLinkUrl linkClass='${myClass}' url='${myUrl}'/>"
+        assertEquals( '<a class="myLinkClass" href="http://www.twitcaps.com/search?q=basket#">http://www.twitcaps.com/search?q=basket#</a>', 
+            applyTemplate(template) )
+    }
+}
